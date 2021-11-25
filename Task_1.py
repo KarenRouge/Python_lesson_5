eea06f7ed518f8bfb4a9cc1f4c8e3e0ab51d4f7f
KarenRouge <karenrouge1991@gmail.com>
2021-11-25 14:08:01 -0500

    Task_1 complete

---
+++ Task_1.py
@@ -0,0 +1,9 @@
+user_str = input(f"Введите информацию: ")
+new_file = open("My_file.txt", "w")
+while True:
+    user_str = input()
+    new_file.write(user_str + '/n')
+    if user_str == ' ':
+        break
+new_file.close
+print("Запись данных завершена.")
