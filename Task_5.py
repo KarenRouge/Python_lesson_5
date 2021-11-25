fb0362879dee39b3c746b4040b1388245352471d
KarenRouge <karenrouge1991@gmail.com>
2021-11-25 18:05:20 -0500

    Task_5 complete

--- Task_5.py
+++ Task_5.py
@@ -1,6 +1,11 @@
-line = (input("Введите числа через пробел: "))
-with open("Task_5.txt", 'w+') as numbers:
-    num = numbers.writelines(line)
-    print(num)
-    num_sp = num.split()
-    print(sum(map(int, num_sp)))
+def func_1():
+    try:
+        with open('Task_5.txt', 'w+') as numbers:
+            line = input('Введите цифры через пробел: ')
+            numbers.writelines(line)
+            num_spl = line.split()
+
+            print(sum(map(int, num_spl)))
+    except ValueError:
+        print('Неправильно набраны числа. Введите целые числа через пробел')
+func_1()
