5ed6996af8b2bed38cd60972d00e230999ece76d
KarenRouge <karenrouge1991@gmail.com>
2021-11-26 15:38:38 -0500

    use

--- Task_1.py
+++ Task_1.py
@@ -2,7 +2,7 @@ user_str = input(f"Введите информацию: ")
 new_file = open("My_file.txt", "w")
 while True:
     user_str = input()
-    new_file.write(user_str + '/n')
+    new_file.write(user_str + '\n')
     if user_str == ' ':
         break
 new_file.close
