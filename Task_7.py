994d4c5de8c85482e17fca278d98f791351fea02
KarenRouge <karenrouge1991@gmail.com>
2021-11-26 12:17:52 -0500

    Task_7 complete

---
+++ Task_7.py
@@ -0,0 +1,19 @@
+with open('Task_7.txt', 'r+') as firm_list:
+    income_dict_firm = {}
+    rub_dict_firm = {}
+    for line in firm_list:
+        parts = line.split()
+        income = int(parts[2]) - int(parts[3])
+        if income > 0:
+            income_dict_firm.update({parts[1]: income})
+        else:
+            rub_dict_firm.update({parts[1]: income})
+    income_dict = {"Average" : str(sum(map(int, income_dict_firm.values())))}
+    income_dict_firm.update(rub_dict_firm)
+    income_dict_firm.update(income_dict)
+import json
+with open('Task_7j.json', 'w') as dict_json:
+    json.dump(income_dict_firm, dict_json)
+
+
+
