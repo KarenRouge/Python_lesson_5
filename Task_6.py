b7cab00a1f359475b0f31906f0ae2f4b319149dc
KarenRouge <karenrouge1991@gmail.com>
2021-11-26 14:14:32 -0500

    Task_6 complete

--- Task_6.py
+++ Task_6.py
@@ -1,7 +1,13 @@
+import re
 
 short_plan = {}
 with open('Task_6.txt', "r", encoding='utf-8') as long_plan:
     for line in long_plan:
-        parts = line.split()
-        
+        line_sp = line.split()
+        disc = line_sp[0]
+        nums = re.findall(r'\d*\.\d+|\d+', line)
+        all_hours = sum(map(int, nums))
+        short_plan.update({disc: all_hours})
+    print(short_plan)
+
 
