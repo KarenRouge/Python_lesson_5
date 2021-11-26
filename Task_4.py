7f50167d7441393a3ac147f3f24e6e117f817a19
KarenRouge <karenrouge1991@gmail.com>
2021-11-26 15:47:55 -0500

    Task_5 complete

--- Task_4.py
+++ Task_4.py
@@ -7,7 +7,6 @@ with open("Task_4.txt", "r+", encoding="utf-8") as eng_numbers:
         line_list.pop(0)
         line_list.insert(0, rus_numbers[i])
         new_line = ' '.join(line_list)
-        print(new_line)
         new_lines.append(new_line)
         i = i + 1
         if i >= 5:
