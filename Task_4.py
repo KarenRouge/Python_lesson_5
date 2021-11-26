e082c0bb4dadc0e2559463dff8c96d90e419312e
KarenRouge <karenrouge1991@gmail.com>
2021-11-26 11:11:09 -0500

    Task_4 update

--- Task_4.py
+++ Task_4.py
@@ -1,10 +1,18 @@
-from   import next
-rus_numbers = {1 : 'Один', 2 : 'Два', 3 : 'три', 4 : 'четыре'}
+rus_numbers = {1: 'Один', 2: 'Два', 3: 'Три', 4: 'Четыре'}
 with open("Task_4.txt", "r") as eng_numbers:
-    rus_list = {}
+    rus_numbers = []
     eng_numbers_list = eng_numbers.read().split('\n')
-    for str in eng_numbers_list:
-        number = str.split()
+    for number in eng_numbers_list:
+        number_sp = number.split(' вЂ” ')
+        for key in rus_numbers:
+            new_number = str(rus_numbers[key]) + ' - ' + number_sp[1]
+            print(new_number)
+            rus_numbers.append(new_number)
+    print(rus_numbers)
+
+
+
+
 
 
 
